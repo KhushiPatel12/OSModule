80508742abe7cc6ebd9b2ddbaff4fdc8ba3354b4
KhushiPatel12 <92209863+KhushiPatel12@users.noreply.github.com>
2021-10-09 23:51:36 +0530

    OS Module Done

---
+++ Practical2/OSModule.js
@@ -0,0 +1,6 @@
+var os = require("os");
+console.log("OS CPU Architecture: ",os.arch());
+console.log("Free Memory of the System: ",os.freemem());
+console.log("Total Memory of the System: ",os.totalmem());
+console.log("OS Platform: ",os.platform());
+console.log("Information about the current user: ",os.userInfo());
